47ed29ad2e925a50201ae4072ee034b06e8b8465
RS guy <rsguy9791@gmail.com>
2026-04-06 16:19:15 +1200

    add deploy step. Update title

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vuetify from 'vite-plugin-vuetify'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/potion-planner/',
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
